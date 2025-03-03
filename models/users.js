0c5f0401f360ed30a9003dbe253b74820a9bca7b
leochok <stalh.leo@gmail.com>
2025-03-03 17:25:45 +0100

    Réalisation de la route users : Signup, Signin, Recher utilisateur par username, Get all, get by distance (geojson), Update, Unsuscribe, addFake

--- models/users.js
+++ models/users.js
@@ -1,8 +1,51 @@
-const mongoose = require ('mongoose');
-const usersSchema = mongoose.Schema({
-// Here is your schema
+const mongoose = require("mongoose");
 
+const userSchema = mongoose.Schema({
+  infos: {
+    username: String,
+    firstname: String,
+    lastname: String,
+    avatar: String,
+    online: Boolean,
+    location: {
+      type: {
+        type: String,
+        enum: ["Point"],
+        required: true,
+      },
+      coordinates: {
+        type: [Number],
+        required: true,
+      },
+    },
+  },
+  authentification: {
+    email: String,
+    password: String,
+    token: String,
+  },
+  description: {
+    work: String,
+    bio: String,
+  },
+  preferences: {
+    favBuddies: [String],
+    favRestaurant: [String],
+    favFood: [String],
+    hobbies: [String],
+    languages: [String],
+    hollydays: Boolean,
+    lunchtime: [
+      {
+        name: String,
+        start: String,
+        stop: String,
+        worked: Boolean,
+      },
+    ],
+  },
+});
+userSchema.index({ "infos.location" : '2dsphere' })
 
-})
-const Users = mongoose.model('users', usersSchema)
-module.exports = Users;
+const User = mongoose.model("users", userSchema);
+module.exports = User;
