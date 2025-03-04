32ac6352498a4e4001c7e4a0e2869c893fe70785
Eva <gacieva1@gmail.com>
2025-03-04 12:09:09 +0100

    modifs backend router reservations

--- models/reservations.js
+++ models/reservations.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose');
-const Users = require('../models/users')
 
 const reservationsSchema = mongoose.Schema({
 name : String,
 users: [{type: mongoose.Schema.Types.ObjectId, ref:'users'}], 
 date: Date, 
-conversation : {type: mongoose.Schema.Types.ObjectId, ref:'conversations'},
+chats : {type: mongoose.Schema.Types.ObjectId, ref:'conversations'},
 })
 
 const Reservations = mongoose.model('reservations', reservationsSchema)
