a3da7642d94f63e5a1807e96a86c865ec7331c47
Eva <gacieva1@gmail.com>
2025-03-11 17:03:17 +0100

    backend Agenda

---
+++ routes/settings.js
@@ -0,0 +1,9 @@
+var express = require("express");
+var router = express.Router();
+const Users = require("../models/users");
+
+
+//--------- Permet de modifier son mot de passe -----------------
+router.put("/")
+
+//-------- Permet de modifier email -------------------
