1c9222f56122e7d0752d5bbd114433d5375b5ceb
Eva <gacieva1@gmail.com>
2025-03-12 17:43:02 +0100

    frontendAgendaMAJ

--- routes/settings.js
+++ routes/settings.js
@@ -4,6 +4,14 @@ const Users = require("../models/users");
 
 
 //--------- Permet de modifier son mot de passe -----------------
-router.put("/")
-
+router.put("/updatepwd", (req,res) => {
+const {userId, oldPassword, newPassword} = req.body
+if (!userId || !oldPassword || !newPassword) {
+    return res.json({error:"Tous les champs sont requis"})
+}
+const user = Users.findById(userId);
+if (!user)
+    return res.json({error : "Utilisateur introuvable"})
+})
+//PAS TERMINÉ
 //-------- Permet de modifier email -------------------
